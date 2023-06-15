3a9667e2d3ff079c2bb0fbdbd87af9aeb874e06f
Tiago Trentin <tiagoandresisola@gmail.com>
2023-06-15 19:22:24 -0300

    preentrega1

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,19 @@
+import cart from "../assets/cart.png"
+
+const styles = {
+    img: {
+        height: "2rem",
+        width: "auto",
+    },
+    span: {
+        color: "white",
+        paddingLeft: 10,
+    },
+}
+
+export const CartWidget = () => (
+    <>
+        <img src={cart} style={styles.img} alt="changuito"/>
+        <span style={styles.span}>0</span>
+    </>
+) 
